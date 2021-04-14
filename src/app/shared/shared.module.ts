5d5f338a5303f6a068b0a025eb5f1a147ab7a49b
Janchorizo <jancho@usal.es>
2021-04-14 16:52:54 +0200

    Add input component.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { InputComponent } from './input/input.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    ButtonComponent
+    ButtonComponent,
+    InputComponent
   ],
   exports: [
-      ButtonComponent
+      ButtonComponent,
+      InputComponent
   ],
   imports: [
+    FormsModule,
     CommonModule
   ]
 })
