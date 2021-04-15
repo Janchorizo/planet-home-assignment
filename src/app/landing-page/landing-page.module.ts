db87adc0c99704ae34846a99def52918a8754577
Janchorizo <jancho@usal.es>
2021-04-15 10:44:45 +0200

    Add basic layout and navigation for landing page.

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 const routes = [
     {path: '', component: LandingPageComponent}
@@ -15,6 +16,7 @@ export const landingRouting = RouterModule.forChild(routes);
   ],
   imports: [
     CommonModule,
+    SharedModule,
     landingRouting
   ]
 })
