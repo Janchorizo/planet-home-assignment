8d27e2582766156d8f78fb20423caad297891c63
Janchorizo <jancho@usal.es>
2021-04-15 11:05:35 +0200

    Add RouterTestingModule to testBed configuration imports for lazily loaded modules.

--- src/app/account-page/account-page/account-page.component.spec.ts
+++ src/app/account-page/account-page/account-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AccountPageComponent } from './account-page.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AccountPageComponent', () => {
   let component: AccountPageComponent;
@@ -8,7 +9,19 @@ describe('AccountPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AccountPageComponent ]
+      declarations: [ AccountPageComponent ],
+      imports: [
+        RouterTestingModule.withRoutes(
+          [
+            {path: 'app', children: [
+              {
+                  path: 'movies',
+                  loadChildren: () => import('../account-page.module').then(m => m.AccountPageModule)
+              },
+            ]}
+          ]
+        )
+      ]
     })
     .compileComponents();
   });
--- src/app/movies-page/movies-page/movies-page.component.spec.ts
+++ src/app/movies-page/movies-page/movies-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { MoviesPageComponent } from './movies-page.component';
 
 describe('MoviesPageComponent', () => {
@@ -8,7 +8,19 @@ describe('MoviesPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MoviesPageComponent ]
+      declarations: [ MoviesPageComponent ],
+      imports: [
+        RouterTestingModule.withRoutes(
+          [
+            {path: 'app', children: [
+              {
+                  path: 'movies',
+                  loadChildren: () => import('../movies-page.module').then(m => m.MoviesPageModule)
+              },
+            ]}
+          ]
+        )
+      ]
     })
     .compileComponents();
   });
