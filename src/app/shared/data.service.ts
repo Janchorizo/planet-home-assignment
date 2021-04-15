e57afaf977fcffe4abf82fcf2c8bede50ab11a09
Janchorizo <jancho@usal.es>
2021-04-15 16:55:59 +0200

    Add user storage handling to data service.

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,9 +1,42 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { Store } from 'redux'
+import { UserActions } from 'src/redux/userActions';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  appStore: Store;
+  userSubject: BehaviorSubject<object>;
+  unsubscribeStore: CallableFunction;
 
-  constructor() { }
+  constructor(
+      @Inject('AppStore') appStore: Store,
+      private userActions: UserActions
+  ) {
+    const initialState = this.appStore.getState();
+    this.userSubject = new BehaviorSubject(initialState.user);
+
+    this.unsubscribeStore = this.appStore.subscribe(
+      this.handleStoreUpdate.bind(this));
+  }
+
+  private handleStoreUpdate() {
+    const newState = this.appStore.getState();
+    this.userSubject.next(newState.user);
+  }
+
+  getUser(): BehaviorSubject<object> {
+    return this.userSubject;
+  }
+
+  setUser(user) {
+    const payload = {user};
+    this.appStore.dispatch(this.userActions.setUser(payload));
+  }
+
+  clearUser() {
+    this.appStore.dispatch(this.userActions.clearUser({}));
+  }
 }
