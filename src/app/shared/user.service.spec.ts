948ba0933674ad26f890add77d4e2a3b5d7b17a0
Janchorizo <jancho@usal.es>
2021-04-15 17:59:55 +0200

    Add app store providers to spec.

--- src/app/shared/user.service.spec.ts
+++ src/app/shared/user.service.spec.ts
@@ -1,21 +1,28 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { createStore } from 'redux';
 
 import { ApiService } from './api.service';
 import { DataService } from './data.service';
 import { UserService } from './user.service';
-import { HttpClientModule } from '@angular/common/http';
+import { UserActions } from 'src/redux/userActions';
+import { appReducer } from 'src/redux/appReducer';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(() => {
+    const store = createStore(appReducer);
+
     TestBed.configureTestingModule({
         imports:[
             HttpClientModule
         ],
         providers: [
             ApiService,
-            DataService
+            DataService,
+            { provide: 'AppStore', useValue: store },
+            UserActions
         ]
     });
     service = TestBed.inject(UserService);
