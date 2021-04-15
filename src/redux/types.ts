b4d330c462f7e5368949328e785cf5576bb2bab2
Janchorizo <jancho@usal.es>
2021-04-15 16:32:30 +0200

    Add redux types.

---
+++ src/redux/types.ts
@@ -0,0 +1,9 @@
+export interface ReduxAction {
+    type: string;
+    payload: any;
+    error?: boolean;
+}
+
+export function createActionGenerator(type: string): (payload: any, error: boolean)=>ReduxAction {
+	return (payload: any, error=false)=>({type, payload, error});
+}
