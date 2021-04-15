7e76b3d8ab7e8da417bd5f7f8dc5f43a0aa6979a
Janchorizo <jancho@usal.es>
2021-04-15 10:10:41 +0200

    Update account page with page container and basic navigation.

--- src/app/account-page/account-page/account-page.component.ts
+++ src/app/account-page/account-page/account-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-account-page',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-page.component.scss']
 })
 export class AccountPageComponent implements OnInit {
+  focused = false;
+  transitioned = false;
+  router: Router;
 
-  constructor() { }
+  constructor(router: Router){
+    this.router = router;
+  }
 
   ngOnInit(): void {
   }
 
+  onCLick() {
+      this.transitioned = true;
+      setTimeout(() => {
+        this.router.navigate(['/app/movies']);
+      }, 600);
+  }
+
+  handleMouseEnter(){
+      this.focused = true;
+  }
+
+  handleMouseExit(){
+    this.focused = false;
+  }
 }
