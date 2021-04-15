6c43c5cef422553bca7eb6ccd5b32b809341dd94
Janchorizo <jancho@usal.es>
2021-04-15 17:08:21 +0200

    Add providers to testbed configuration in data service specs.

--- src/app/shared/data.service.spec.ts
+++ src/app/shared/data.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { createStore } from 'redux';
 
 import { DataService } from './data.service';
+import { UserActions } from 'src/redux/userActions';
+import { appReducer } from 'src/redux/appReducer';
 
 describe('DataService', () => {
   let service: DataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const store = createStore(appReducer);
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: 'AppStore', useValue: store },
+        UserActions
+      ]
+    });
     service = TestBed.inject(DataService);
   });
 
