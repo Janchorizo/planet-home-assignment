29fc5be7ad4adba191e9b4b863c7a68c5f2a95db
Janchorizo <jancho@usal.es>
2021-04-15 17:46:14 +0200

    Add imports to user service specs.

--- src/app/shared/user.service.spec.ts
+++ src/app/shared/user.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
+import { ApiService } from './api.service';
+import { DataService } from './data.service';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+        imports: [
+            ApiService,
+            DataService
+        ]
+    });
     service = TestBed.inject(UserService);
   });
 
