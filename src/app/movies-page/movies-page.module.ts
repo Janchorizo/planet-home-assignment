396bcff6c2e27b80b7129c16944ea9d169a79220
Janchorizo <jancho@usal.es>
2021-04-15 10:44:26 +0200

    Add basic navigation for movies page.

--- src/app/movies-page/movies-page.module.ts
+++ src/app/movies-page/movies-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MoviesPageComponent } from './movies-page/movies-page.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 const routes = [
     {path: '', component: MoviesPageComponent}
@@ -15,6 +16,7 @@ export const moviesRouting = RouterModule.forChild(routes);
   ],
   imports: [
     CommonModule,
+    SharedModule,
     moviesRouting
   ]
 })
--- src/app/movies-page/movies-page/movies-page.component.ts
+++ src/app/movies-page/movies-page/movies-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movies-page',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies-page.component.scss']
 })
 export class MoviesPageComponent implements OnInit {
+    focused = false;
+    transitioned = false;
+    altBgColor = 'white';
+    router: Router;
+  
+    constructor(router: Router){
+      this.router = router;
+    }
+  
+    ngOnInit(): void {
+    }
+  
+    handleMouseEnter(){
+        this.focused = true;
+    }
+  
+    handleMouseExit(){
+      this.focused = false;
+    }
 
-  constructor() { }
+    handleAccountMouseEnter() {
+        this.altBgColor = '#9289c8';
+        this.focused = true;
+    }
 
-  ngOnInit(): void {
-  }
+    handleAcountClick() {
+        this.transitioned = true;
+        setTimeout(() => {
+          this.router.navigate(['/app/account']);
+        }, 600);
+    }
 
+    handleLogoutMouseEnter() {
+        this.altBgColor = 'white';
+        this.focused = true;
+    }
+
+    handleLogoutClick() {
+        this.transitioned = true;
+        setTimeout(() => {
+          this.router.navigate(['/']);
+        }, 600);
+    }
+
+    handleMouseLeave() {
+        this.focused = false;
+    }
 }
