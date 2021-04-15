b07e91ec61eb2f06c3e1c4cd1485f3a2060a1b1d
Janchorizo <jancho@usal.es>
2021-04-15 17:56:03 +0200

    Add http to specs import.

--- src/app/shared/user.service.spec.ts
+++ src/app/shared/user.service.spec.ts
@@ -3,12 +3,16 @@ import { TestBed } from '@angular/core/testing';
 import { ApiService } from './api.service';
 import { DataService } from './data.service';
 import { UserService } from './user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+        imports:[
+            HttpClientModule
+        ],
         providers: [
             ApiService,
             DataService
