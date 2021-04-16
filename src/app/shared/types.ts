6706da270b1b29b5380fe0e2e82f83f29b2838cb
Janchorizo <jancho@usal.es>
2021-04-16 12:56:08 +0200

    Create movie type.

--- src/app/shared/types.ts
+++ src/app/shared/types.ts
@@ -20,3 +20,18 @@ export interface User {
   id: string;
   token: string;
 }
+
+export interface Movie {
+  id: string,
+  title: string,
+  year: number,
+  published_at: string,
+  duration: number,
+  country: string,
+  language: string,
+  director: string,
+  description: string,
+  budget: number,
+  url: string,
+  poster_url: string
+}
