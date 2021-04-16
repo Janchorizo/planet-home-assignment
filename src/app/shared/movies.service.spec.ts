1f8a12e61aadaeb2b1d90603b3984c585030ae2a
Janchorizo <jancho@usal.es>
2021-04-16 17:13:08 +0200

    Add extra imports for spec configuration.

--- src/app/shared/movies.service.spec.ts
+++ src/app/shared/movies.service.spec.ts
@@ -1,12 +1,31 @@
 import { TestBed } from '@angular/core/testing';
-
+import { createStore } from 'redux';
 import { MoviesService } from './movies.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './api.service';
+import { DataService } from './data.service';
+import { UserActions } from 'src/redux/userActions';
+import { MoviesActions } from 'src/redux/moviesActions';
+import { appReducer } from 'src/redux/appReducer';
 
 describe('MoviesService', () => {
   let service: MoviesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const store = createStore(appReducer);
+
+    TestBed.configureTestingModule({
+      imports:[
+          HttpClientModule
+      ],
+      providers: [
+          ApiService,
+          DataService,
+          { provide: 'AppStore', useValue: store },
+          UserActions,
+          MoviesActions
+      ]
+  });
     service = TestBed.inject(MoviesService);
   });
 
