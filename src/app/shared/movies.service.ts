7589d6db2176cbc23c9b67d5d4980c050ea1d90f
Janchorizo <jancho@usal.es>
2021-04-16 12:39:12 +0200

    Implement movies service.

--- src/app/shared/movies.service.ts
+++ src/app/shared/movies.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { DataService } from './data.service';
+import { User } from './types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
+  user: User|null;
+  pageSize = 6;
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService,
+    private dataService: DataService,
+  ) {
+    this.dataService.getUser().subscribe(
+      (user: User) => { this.user = user; }
+    );
+  }
+
+  getRatedMovies() {
+    return this.dataService.getRatedMovies();
+  }
+
+  getUnratedMovies() {
+    return this.dataService.getUnratedMovies();
+  }
+
+  fetchRatedMoviesPage(page: number) {
+    this.apiService.hit(
+      'getRatedMovies',
+      null,
+      {page, size: this.pageSize},
+      null,
+      this.user?.token
+    ).subscribe(
+      response => {
+        if (response.status === 200) {
+          this.dataService.changeRatedMoviesPage(page, response.body);
+        }
+      }
+    );
+  }
+
+  fetchUnratedMoviesPage(page: number) {
+    this.apiService.hit(
+      'getUnratedMovies',
+      null,
+      {page, size: this.pageSize},
+      null,
+      this.user?.token
+    ).subscribe(
+      response => {
+        if (response.status === 200) {
+          this.dataService.changeUnratedMoviesPage(page, response.body);
+        }
+      }
+    );
+  }
 }
