beb28cd9e947f151550c019167f04643fbfe95b9
Janchorizo <jancho@usal.es>
2021-04-16 15:05:34 +0200

    Add SET_RATINGS action for movie store.

--- src/redux/moviesActions.ts
+++ src/redux/moviesActions.ts
@@ -6,6 +6,7 @@ export const SET_UNRATED_PAGE = 'SET_UNRATED_PAGE';
 export const SET_UNRATED = 'SET_UNRATED';
 export const SET_RATED_PAGE = 'SET_RATED_PAGE';
 export const SET_RATED = 'SET_RATED';
+export const SET_RATINGS = 'SET_RATINGS';
 
 // Action generators
 export const setRatedPage: (payload: any, error: boolean)=>ReduxAction =
@@ -20,10 +21,14 @@ export const setUnratedPage: (payload: any, error: boolean)=>ReduxAction =
 export const setUnratedMovies: (payload: any, error: boolean)=>ReduxAction =
   createActionGenerator(SET_UNRATED);
 
+export const setRatings: (payload: any, error: boolean)=>ReduxAction =
+  createActionGenerator(SET_RATINGS);
+
 // For bootstrapping purposes
 export class MoviesActions {
   setRatedPage: (payload: any, error?: boolean)=>ReduxAction = setRatedPage;
   setRatedMovies: (payload: any, error?: boolean)=>ReduxAction = setRatedMovies;
   setUnratedPage: (payload: any, error?: boolean)=>ReduxAction = setUnratedPage;
   setUnratedMovies: (payload: any, error?: boolean)=>ReduxAction = setUnratedMovies;
+  setRatings: (payload: any, error?: boolean)=>ReduxAction = setRatings;
 }
