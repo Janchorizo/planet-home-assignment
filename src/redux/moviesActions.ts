8139c6fb22ec46861af43c12905d21c56f39166f
Janchorizo <jancho@usal.es>
2021-04-16 11:55:19 +0200

    Create redux actions for movies.

---
+++ src/redux/moviesActions.ts
@@ -0,0 +1,29 @@
+import { ReduxAction, createActionGenerator } from './types';
+
+
+// Action types
+export const SET_UNRATED_PAGE = 'SET_UNRATED_PAGE';
+export const SET_UNRATED = 'SET_UNRATED';
+export const SET_RATED_PAGE = 'SET_RATED_PAGE';
+export const SET_RATED = 'SET_RATED';
+
+// Action generators
+export const setRatedPage: (payload: any, error: boolean)=>ReduxAction =
+  createActionGenerator(SET_RATED_PAGE);
+
+export const setRatedMovies: (payload: any, error: boolean)=>ReduxAction =
+  createActionGenerator(SET_RATED);
+
+export const setUnratedPage: (payload: any, error: boolean)=>ReduxAction =
+  createActionGenerator(SET_UNRATED_PAGE);
+
+export const setUnratedMovies: (payload: any, error: boolean)=>ReduxAction =
+  createActionGenerator(SET_UNRATED);
+
+// For bootstrapping purposes
+export class MoviesActions {
+  setRatedPage: (payload: any, error: boolean)=>ReduxAction = setRatedPage;
+  setRatedMovies: (payload: any, error: boolean)=>ReduxAction = setRatedMovies;
+  setUnratedPage: (payload: any, error: boolean)=>ReduxAction = setUnratedPage;
+  setUnratedMovies: (payload: any, error: boolean)=>ReduxAction = setUnratedMovies;
+}
