3b73d509f70ee162bf0e74907cb0b2d9dd970056
Janchorizo <jancho@usal.es>
2021-04-16 10:15:00 +0200

    Add necessary imports to specs.

--- src/app/landing-page/user-access/user-access.component.spec.ts
+++ src/app/landing-page/user-access/user-access.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserAccessComponent } from './user-access.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('UserAccessComponent', () => {
   let component: UserAccessComponent;
@@ -8,7 +11,16 @@ describe('UserAccessComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserAccessComponent ]
+      declarations: [ UserAccessComponent ],
+      imports: [
+        HttpClientModule,
+        SharedModule,
+        RouterTestingModule.withRoutes(
+          [
+            {path: '', component: UserAccessComponent}
+          ]
+        )
+      ],
     })
     .compileComponents();
   });
